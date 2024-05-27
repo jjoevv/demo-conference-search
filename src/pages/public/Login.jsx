bf2d06c4a23f58256af613deea0faff39e89d7c0
Ha Nguyen Thao Vy <112704960+jjoevv@users.noreply.github.com>
2024-05-27 12:34:05 +0700

    fix login

--- src/pages/public/Login.jsx
+++ src/pages/public/Login.jsx
@@ -8,7 +8,7 @@ import Loading from '../../components/Loading'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 const Login = () => {
-    const {loading, handleLogin } = useAuth();
+    const { loading, handleLogin } = useAuth();
     const [message, setMessage] = useState('')
     const [status, setStatus] = useState(false)
     const [error, setError] = useState(null);
@@ -26,36 +26,36 @@ const Login = () => {
     }
     const validateForm = () => {
         for (let key in account) {
-          if (!account[key]) {
-            return `${key} cannot be empty.`;
-          }
+            if (!account[key]) {
+                return `${key} cannot be empty.`;
+            }
         }
         if (account.password !== account.confirm) {
-          return "Passwords do not match.";
+            return "Passwords do not match.";
         }
         return null;
-      };
+    };
     const handleSubmit = async (e) => {
         e.preventDefault()
-        
+
         setIsSubmit(true)
         const errorMsg = validateForm();
         if (errorMsg) {
-        setError(errorMsg);
-        return;
+            setError(errorMsg);
+            return;
         } else {
-        setError(null);
-        const result = await handleLogin(account.email, account.password)
-        
-        setStatus(result.status)
-        setMessage(result.message)
+            setError(null);
+            const result = await handleLogin(account.email, account.password)
 
-        if(result.status){
-            setIsSubmit(false)
-            handleLogin(account.email, account.password)            
+            setStatus(result.status)
+            setMessage(result.message)
+
+            if (result.status) {
+                setIsSubmit(false)
+                handleLogin(account.email, account.password)
+            }
         }
-        }
-        
+
     }
     const chooseSignup = () => {
         navigate('/signup')
@@ -66,23 +66,23 @@ const Login = () => {
     return (
         <Container className="100-w 100-h min-vh-100 d-flex justify-content-center align-items-center text-center" fluid={true} style={{ backgroundColor: "#C2F1EB" }} >
             <Row className='bg-white rounded-4 d-flex box-area w-75 h-50 mx-auto'>
-                
+
                 <Col className='p-1 d-flex flex-column align-items-center justify-content-center bg-skyblue-light rounded-start-4' >
 
                     <Form className='w-75 p-5 bg'>
-                        <h1 className='mb-4 fw-bold' style={{fontSize: "30px", color: "#419489"}}>Log In</h1>
-                        <Button className='border-0 w-100 p-2 rounded-3' style={{backgroundColor: "#E8F1F3", color: "#434343"}}>
-                            <Image src={googleIcon} width={20} className="me-2"/>
+                        <h1 className='mb-4 fw-bold' style={{ fontSize: "30px", color: "#419489" }}>Log In</h1>
+                        <Button className='border-0 w-100 p-2 rounded-3' style={{ backgroundColor: "#E8F1F3", color: "#434343" }}>
+                            <Image src={googleIcon} width={20} className="me-2" />
                             You can join with your Google account</Button>
                         <div className='d-flex flex-row align-items-center'>
-                            <div style={{flex: 1, height: '1px', backgroundColor: 'black'}} />
+                            <div style={{ flex: 1, height: '1px', backgroundColor: 'black' }} />
 
                             <div><p className='pt-3 mx-3'>or log in with your account</p></div>
 
-                            <div style={{flex: 1, height: '1px', backgroundColor: 'gray'}} />
+                            <div style={{ flex: 1, height: '1px', backgroundColor: 'gray' }} />
                         </div>
                         <Form.Group className="mb-3 text-start">
-                            <Form.Label htmlFor="inputPassword5" style={{fontSize: "20px", color: "#434343"}}>Email</Form.Label>
+                            <Form.Label htmlFor="inputPassword5" style={{ fontSize: "20px", color: "#434343" }}>Email</Form.Label>
                             <Form.Control
                                 type="text"
                                 name="email"
@@ -94,7 +94,7 @@ const Login = () => {
                             />
                         </Form.Group>
                         <Form.Group className="mb-4 text-start">
-                        <Form.Label htmlFor="inputPassword5" style={{fontSize: "20px", color: "#434343"}}>Password</Form.Label>  
+                            <Form.Label htmlFor="inputPassword5" style={{ fontSize: "20px", color: "#434343" }}>Password</Form.Label>
                             <InputGroup>
                                 <Form.Control
                                     type={showpassword ? "text" : "password"}
@@ -105,13 +105,13 @@ const Login = () => {
                                     required
                                     className="border-0 shadow-sm rounded-start-2 px-3 py-2"
                                 />
-                                 <InputGroup.Text onClick={toggleShowPassword} className='border-0 bg-white rounded-end shadow-sm '>
-                                        {showpassword ? <FontAwesomeIcon icon={faEye}/> : <FontAwesomeIcon icon={faEyeSlash}/>}
-                                    </InputGroup.Text>
+                                <InputGroup.Text onClick={toggleShowPassword} className='border-0 bg-white rounded-end shadow-sm '>
+                                    {showpassword ? <FontAwesomeIcon icon={faEye} /> : <FontAwesomeIcon icon={faEyeSlash} />}
+                                </InputGroup.Text>
                             </InputGroup>
                         </Form.Group>
                         <ButtonGroup className="d-flex justify-content-around mb-3">
-                            <Button className='bg-transparent border-0 p-0' style={{color: "#434343"}}>Forgot your password?</Button>
+                            <Button className='bg-transparent border-0 p-0' style={{ color: "#434343" }}>Forgot your password?</Button>
                         </ButtonGroup>
                         {
                             isSubmit && error && <p className="text-warning">{error}</p>
@@ -120,33 +120,33 @@ const Login = () => {
                             !status && isSubmit && message !== '' && <p className="text-danger">{message}</p>
                         }
                         <div className='d-flex flex-column justify-content-center align-items-center'>
-                            {error && <span className='text-danger'>{error}</span>}
                             
-                                <Button
-                                    onClick={handleSubmit}
-                                    className='border-0 fw-bold rounded-4 p-2'
-                                    style={{width: "140px", fontSize: "20px", backgroundColor: "#419489"}}>
-                            
-                                    {
-                                        isSubmit && loading ?
-                                        <Loading/>
+
+                            <Button
+                                onClick={handleSubmit}
+                                className='border-0 fw-bold rounded-4 p-2'
+                                style={{ width: "140px", fontSize: "20px", backgroundColor: "#419489" }}>
+
+                                {
+                                    isSubmit && loading ?
+                                        <Loading />
                                         :
                                         "LOG IN"
-                                    }
-                                </Button>
+                                }
+                            </Button>
                         </div>
                         <div className="my-3 mx-3 d-flex align-items-center justify-content-center">
                             <span htmlFor="#signup">{`Don't have an account?`}</span>
                             <Button
                                 className='bg-transparent border-0 fw-bold p-0 m-0 w-25'
-                                style={{fontSize: "20px", color: "#419489"}}
+                                style={{ fontSize: "20px", color: "#419489" }}
                                 onClick={chooseSignup}>
                                 SIGN UP
                             </Button>
                         </div>
                     </Form>
                 </Col>
-                <Col sm={4} className='d-flex flex-column align-items-center justify-content-center rounded-end-4 text-light' style={{backgroundColor: "#419489"}}>
+                <Col sm={4} className='d-flex flex-column align-items-center justify-content-center rounded-end-4 text-light' style={{ backgroundColor: "#419489" }}>
                     <span className='h1 mb-2'>Welcome Back!</span>
                     <span className='mt-3'>Enter your personal account and keep following conferences/magazines!</span>
 
@@ -155,7 +155,7 @@ const Login = () => {
                     </div>
                 </Col>
             </Row>
-            
+
         </Container>
     )
 }
