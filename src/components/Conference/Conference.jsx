55e84cad26936a4ff29ba6361470a629425d9dde
Ha Nguyen Thao Vy <112704960+jjoevv@users.noreply.github.com>
2024-06-08 21:32:03 +0700

    fix page

--- src/components/Conference/Conference.jsx
+++ src/components/Conference/Conference.jsx
@@ -150,7 +150,7 @@ const Conference = ({ conferencesProp, loading, totalPages, onReload, totalConfe
     };
 
     const chooseConf = async (id) => {
-        //await handleGetOne(id)
+        await handleGetOne(id)
         navigate(`/detailed-information/${id}`)
     }
 
